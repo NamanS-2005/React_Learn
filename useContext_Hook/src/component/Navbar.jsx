96e84d44dbfa2edfb5c16601bafc1db65175ac94
NamanS-2005 <2005namvan@gmail.com>
2025-04-09 18:13:37 +0530

    Use Context Hook

---
+++ useContext_Hook/src/component/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from './Button'
+
+
+const navbar = () => {
+  return (
+    <>
+    <div>
+      Navbar
+    </div>
+    <Button/>
+    </>
+  )
+}
+
+export default navbar
