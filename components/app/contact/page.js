2b3dd120f445721eb654fd8eb27bdabeeaa09a58
NamanS-2005 <2005namvan@gmail.com>
2025-06-10 16:43:03 +0530

    deeper knowledge of server components

---
+++ components/app/contact/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      Contact
+    </div>
+  )
+}
+
+export default contact
+
+export const metadata = {
+  title: "Contact Facebook - Connect with the world",
+  description: "This is facebook and we can connect with the world using facebook",
+};
