af185200e55ec1b8fca5e18d75b63603bde9655e
NamanS-2005 <2005namvan@gmail.com>
2025-06-10 16:40:51 +0530

    understood server components

---
+++ ServerComponents/my-app/app/page.js
@@ -0,0 +1,17 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <div>
+
+      I am a component {count}
+      </div>
+      <button onClick={()=> setCount(count + 1)} > INC (+) </button>
+    </div>
+  );
+}
